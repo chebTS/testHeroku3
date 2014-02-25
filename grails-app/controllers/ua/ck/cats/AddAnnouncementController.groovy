80ba16309e92d705719e72befc78972bd140a49e
Cheb <chebTS@gmail.com>
2014-02-25 14:02:26 -0800

    Adding announcents added

--- grails-app/controllers/ua/ck/cats/AddAnnouncementController.groovy
+++ grails-app/controllers/ua/ck/cats/AddAnnouncementController.groovy
@@ -6,11 +6,14 @@ class AddAnnouncementController {
 		
 		if (params.containsKey("title") && params.containsKey("category")){
 			String title = params.get("title", "default")
-			String category = params.get("category","default")
-			def ann = new Announcement(title: title, category: category )
+			int category = params.get("category","default")
+			//def ann = new Announcement(title: title, category: category )
+			def ann = new Announcement(title, category, true, "bla bla", 4.4d, 5.5d, "Cherkassy", 1392766862).save(failOnError: true)
 			ann.save()
 		}else{		
 			
 		}
 	}
 }
+
+//new Announcement("Lost cat", 1, true, "bla bla", 4.4d, 5.5d, "Cherkassy", 1392766862).save(failOnError: true)
